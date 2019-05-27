446a483553307982c684a14f15b76377e5e789fd
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-27 14:33:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
   return "I LOVE YOU TOO PUMPKIN!"
+elseif phrase == phrase.capitalize
+  return "HUH?! SPEAK UP, SONNY!"
 #  puts "HUH?! SPEAK UP, SONNY!"
 #elseif phrase == phrase.upcase
 #  puts "NO, NOT SINCE 1938!"
