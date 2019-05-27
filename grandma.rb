7bc7a1e1ddf4430c70bbd040c8b4d88cf4fe4736
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-27 14:31:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
   puts "I LOVE YOU TOO PUMPKIN!"
-  puts "HUH?! SPEAK UP, SONNY!"
+#  puts "HUH?! SPEAK UP, SONNY!"
 #elseif phrase == phrase.upcase
 #  puts "NO, NOT SINCE 1938!"
 #elseif phrase == "I LOVE YOU GRANDMA!"
